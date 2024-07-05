48d1b49bbb56001f97861d7186d4f76e4ad110e1
JananiKugarajeevan <janani22thangavel@gmail.com>
2024-07-05 09:54:44 +0530

    Update analysis_results.py

--- tracking/analysis_results.py
+++ tracking/analysis_results.py
@@ -8,14 +8,14 @@ from lib.test.evaluation import get_dataset, trackerlist
 trackers = []
 dataset_name = 'lasot'
 
-trackers.extend(trackerlist(name='oiftrack', parameter_name='vitb_256_mae_ce_all_dataset', dataset_name=dataset_name,
+trackers.extend(trackerlist(name='siftrack', parameter_name='vitb_256_mae_ce_all_dataset', dataset_name=dataset_name,
                             run_ids=None, display_name=''))
 dataset = get_dataset(dataset_name)
 print_results(trackers, dataset, dataset_name, merge_results=True, plot_types=('success', 'norm_prec', 'prec'))
 
 dataset = get_dataset("uav")
 
-trackers.extend(trackerlist(name='oiftrack', parameter_name='vitb_256_mae_ce_all_dataset', dataset_name=dataset_name,
+trackers.extend(trackerlist(name='siftrack', parameter_name='vitb_256_mae_ce_all_dataset', dataset_name=dataset_name,
                             run_ids=None, display_name=''))
 
 print_results(trackers, dataset, 'uav', merge_results=True, plot_types=('success', 'prec'))
